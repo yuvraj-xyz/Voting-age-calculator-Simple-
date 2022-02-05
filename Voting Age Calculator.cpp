6dd4c3c5854fc4b9da581fe57d3cb5059693e9c9
yuvraj.cpp <78093228+yuvicpp@users.noreply.github.com>
2022-02-05 07:57:44 -0600

    Update Voting Age Calculator.cpp

--- Voting Age Calculator.cpp
+++ Voting Age Calculator.cpp
@@ -8,15 +8,21 @@ int main() {
 
     int differ = 18 - age;
 
-    if (age<=16) // valid age for Voting in India is 18 or Above 18
+    if (age>150 || age<1)
+    {
+        cout << "Invalid age";
+    }
+
+    else if (age<=16) // valid age for Voting in India is 18 or Above 18
     {
         cout << "You Can't Vote Yet" "\n";
-        cout << "You can Vote in " << differ << " Years" "\n";
+        cout << "But You can Vote in " << differ << " Years" "\n";
     }
 
     else if (age=16 && differ==1)
     {
-        cout << "You can Vode in " << differ << " Year" "\n";
+        cout << "You Can't Vote Yet" "\n";
+        cout << "But You can Vote in " << differ << " Year" "\n";
     }
     
     else 
